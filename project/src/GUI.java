97f4263d9c00b76607837df428f08c1a4b772c42
mati <matyrios2015@gmail.com>
2019-09-10 22:29:58 -0300

    añadido personaje y correccion en GUI

--- project/src/GUI.java
+++ project/src/GUI.java
@@ -13,22 +13,22 @@ import javax.swing.ImageIcon;
 		private static final long serialVersionUID = 1L;
 
 		private JPanel contentPane;
-		private JLabel[][] botones;
+		private JButton[][] botones;
 		private JLabel dibujo;
-		private JPanel panelLabels;
+		private JPanel panelBotones;
 		
 		public GUI() {
 			contentPane = new JPanel();
 			setContentPane(contentPane);
-			panelLabels= new JPanel();
+			panelBotones= new JPanel();
 			
 			getContentPane().setLayout(null);
-			panelLabels.setLayout(null);
+			panelBotones.setLayout(null);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			panelLabels.setBounds(0,0,1024,560);
-			setBounds(150, 0, 1024, 768);
+			panelBotones.setBounds(0,0,1024,580);
+			setBounds(150, 0, 1035, 768);//esto quedo asi porque con 1024 no se ve el borde
 			
-			panelLabels.setOpaque(false);
+			panelBotones.setOpaque(false);
 			inicializarLabels();
 			agregarDibujo();
 
@@ -43,26 +43,26 @@ import javax.swing.ImageIcon;
 		}
 		
 		private void agregarDibujo(){
-			ImageIcon imagen = new ImageIcon(this.getClass().getResource("sprites\\suelo2.png"));
+			ImageIcon imagen = new ImageIcon(this.getClass().getResource("sprites\\sueloMapa.png"));
 			dibujo = new JLabel(imagen);
 			dibujo.setOpaque(false);
 			contentPane.add(dibujo);
-			dibujo.setBounds(0, 0,1024,580);
+			dibujo.setBounds(0, 0,1024,575);
 		}
 		
 		private void inicializarLabels() {
-			botones= new JLabel[6][10];
+			botones= new JButton[6][10];
 			for (int i=0; i<botones.length;i++)
 				for (int j=0; j<botones[0].length;j++) {
-					botones[i][j]=new JLabel();
-					botones[i][j].setIcon(new ImageIcon(this.getClass().getResource("sprites\\torre4.png")));
+					botones[i][j]=new JButton();
+					botones[i][j].setIcon(new ImageIcon(this.getClass().getResource("sprites\\isaac.gif")));
 					botones[i][j].setOpaque(false);
-					botones[i][j].setSize(62, 62);
-					panelLabels.add(botones[i][j]);
-					//contentPane.add(botones[i][j]);
-					botones[i][j].setLocation(i*(1024/6), j*(580/6));
+					botones[i][j].setContentAreaFilled(false);
+					botones[i][j].setSize(103, 97);
+					panelBotones.add(botones[i][j]);
+					botones[i][j].setLocation(j*(1024/10), i*(580/6));
 				}
-			contentPane.add(panelLabels);
+			contentPane.add(panelBotones);
 					
 		}
 }
