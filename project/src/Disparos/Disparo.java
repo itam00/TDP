8f763f611a2c677d94c7b1a3e897272f30e8e02c
mati <matyrios2015@gmail.com>
2019-11-02 14:01:08 -0300

    error disparo corregido

--- project/src/Disparos/Disparo.java
+++ project/src/Disparos/Disparo.java
@@ -14,7 +14,7 @@ public abstract class Disparo extends Elemento {
 	public Disparo(int x, int y, Mapa m,int danio, float rango) {
 		super(x, y,m);
 		this.danio = danio;
-		this.rango=rango;
+		this.rango=rango*102;
 	}
 	
 	public int getDanio() {
--- project/src/Graficos/Torre5Grafica.java
+++ project/src/Graficos/Torre5Grafica.java
@@ -5,8 +5,8 @@ import javax.swing.ImageIcon;
 public class Torre5Grafica extends ElementoGrafico{
 	public Torre5Grafica() {
 		super();
-		width  =103;
-		height = 180;
+		width  =180;
+		height = 103;
 		image = new ImageIcon(getClass().getResource("/Sprites/torre5.gif"));
 		this.setIcon(image);
 	}
