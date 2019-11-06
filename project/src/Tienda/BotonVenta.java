5f9e2a48759602efdde264c38d890bb50feccf70
alanrychert <alanrychert@gmail.com>
2019-11-06 19:27:56 -0300

    VisitorVenta

---
+++ project/src/Tienda/BotonVenta.java
@@ -0,0 +1,32 @@
+package Tienda;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import Personajes.Torre1;
+
+public class BotonVenta extends JButton {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected Tienda tienda;
+
+	public BotonVenta (Tienda t) {
+		tienda=t;
+		
+		this.setText("Vender");
+		
+		this.addActionListener(new ActionListener() {
+	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tienda.quiereVender();
+			}
+			
+			});
+		}
+}
