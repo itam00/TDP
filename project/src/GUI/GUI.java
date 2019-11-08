eeb353aa6b4c94bc79b27001730de1ca4b30e3fa
mati <matyrios2015@gmail.com>
2019-11-07 21:42:10 -0300

    charco agregado

--- project/src/GUI/GUI.java
+++ project/src/GUI/GUI.java
@@ -118,7 +118,8 @@ public class GUI extends JFrame implements MouseListener{
 		panelJuego.add(grafico);
 		float x = e.getX();
 		float y = e.getY();
-		grafico.setBounds((int)x,(int)y, grafico.getAlto(), grafico.getAncho());
+		//grafico.setBounds((int)x,(int)y, grafico.getAlto(), grafico.getAncho());
+		grafico.setSize(grafico.getAlto(), grafico.getAncho());
 		panelJuego.setComponentZOrder(grafico, 0);
 	}
 
